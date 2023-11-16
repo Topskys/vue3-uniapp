ded3a73f4f5704b19f32fb937ed8bd3f47a9f5df
Topskys <2477350343@qq.com>
2023-11-16 22:15:14 +0800

    fix：修改热门推荐页面样式

--- heima-shop/src/pages/hot/hot.vue
+++ heima-shop/src/pages/hot/hot.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { getHotRecommend } from '@/services/hot';
 import type { SubTypeItem } from '@/types/hot';
+import { onLoad } from '@dcloudio/uni-app';
 import { ref, reactive, onMounted } from 'vue';
 
 // init state
@@ -11,8 +12,8 @@ const urlMap = [
   { type: '4', title: '新鲜好物', url: '/hot/new' },
 ]
 const bannerPicture = ref(''); // 广告图片
-const subTypes = ref<SubTypeItem[]>([]); // 推荐选项
-const activeIndex = ref(0)
+const subTypes = ref<(SubTypeItem & { finish?: boolean })[]>([]); // 推荐选项
+const activeIndex = ref(0); // 当前选中的选项
 
 
 // uniapp获取页面参数
@@ -22,9 +23,12 @@ const query = defineProps<{ type: string }>();
 const currUrlMap = urlMap.find(v => v.type === query.type);
 uni.setNavigationBarTitle({ title: currUrlMap!.title });
 
-
+// 获取推荐数据
 const getHotRecommendData = async () => {
-  const res = await getHotRecommend(currUrlMap!.url);
+  const res = await getHotRecommend(currUrlMap!.url, {
+    page: import.meta.env.DEV ? 30 : 1,
+    pageSize: 10,
+  });
   bannerPicture.value = res.result.bannerPicture;
   subTypes.value = res.result.subTypes;
 }
@@ -34,19 +38,30 @@ const onScrolltolower = async () => {
   // 获取当前选项
   const currSubType = subTypes.value[activeIndex.value];
   // 当前页码累加
-  currSubType.goodsItems.page++;
+  if (currSubType.goodsItems.page < currSubType.goodsItems.pages) {
+    currSubType.goodsItems.page++;
+  } else {
+    currSubType.finish = true;
+    return uni.showToast({ icon: 'none', title: '没有更多数据了~' });
+  }
   // 请求数据
   const res = await getHotRecommend(currUrlMap!.url, {
     subType: currSubType.id,
     page: currSubType.goodsItems.page,
     pageSize: currSubType.goodsItems.pageSize
   });
+  // 提取新数据
+  const newSubTypes = res.result.subTypes[activeIndex.value];
+  // 当前选项数组追加
+  currSubType.goodsItems.items.push(...newSubTypes.goodsItems.items);
 }
 
 
 
 
-onMounted(() => { });
+onLoad(() => {
+  getHotRecommendData();
+});
 
 
 </script>
@@ -55,7 +70,7 @@ onMounted(() => { });
   <view class="viewport">
     <!-- recommend cover -->
     <view class="cover">
-      <image :src="bannerPicture"></image>
+      <image :src="bannerPicture" class="image"></image>
     </view>
     <!-- tabs -->
     <view class="tabs">
@@ -77,31 +92,82 @@ onMounted(() => { });
           </view>
         </navigator>
       </view>
-      <view class="loading-text">正在加载...</view>
+      <view class="loading-text">{{ item.finish ? '没有更多数据了~' : '正在加载...' }}</view>
     </scroll-view>
   </view>
 </template>
 
 <style scoped lang="scss">
-.scroll-view {
-  flex: 1;
+page {
+  height: 100%;
+  background-color: #ffffff;
 }
 
 .viewport {
-  .thumb {
-    width: 305rpx;
-    height: 305rpx;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  padding: 270rpx 0 0;
+  position: relative;
+
+  .cover {
+    position: absolute;
+    top: 0;
+    width: 100%;
+    .image {
+      width: 100%;
+      height: 210rpx;
+    }
+
   }
 
-  .name {
-    height: 88rpx;
-    font-size: 26rpx;
+  .tabs {
+    position: absolute;
+    top: 150rpx;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 10;
+    width: 720rpx;
+    height: 60rpx;
+    padding: 60rpx 30rpx;
+    font-size: 28rpx;
+    border-radius: 10rpx;
+    border: 1rpx solid #eee;
+    background-color: #ffffff;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
-  .price {
-    line-height: 1;
-    color: #cf4444;
-    font-size: 30rpx;
+  .goods {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20rpx;
+
+    .navigator {
+      .thumb {
+        width: 305rpx;
+        height: 305rpx;
+      }
+
+      .name {
+        height: 88rpx;
+        font-size: 26rpx;
+        margin: 10rpx 0;
+        color: #262626;
+        font-size: 28rpx;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+
+      .price {
+        line-height: 1;
+        color: #cf4444;
+        font-size: 30rpx;
+      }
+    }
   }
 
   .symbol,
@@ -109,6 +175,7 @@ onMounted(() => { });
     font-size: 70%;
   }
 }
+
 
 .loading-text {
   text-align: center;
